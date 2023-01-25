f5d1a743e99152cf3a970c2bef5331e8c7442171
Daniel Zamorano <zdaniel655@gmail.com>
2023-01-25 04:38:46 -0500

    Se crean los datasources y modelos para el proyecto mascota

---
+++ src/models/mascota.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    default: q,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  identificador: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'date',
+  })
+  fecha_nacimiento?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
